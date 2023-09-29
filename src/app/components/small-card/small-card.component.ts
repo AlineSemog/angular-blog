90ff80033e1e969adac34652d0796a02839cdc1e
Aline <likasemog@gmail.com>
2023-09-29 15:12:57 -0300

    content created

--- src/app/components/small-card/small-card.component.ts
+++ src/app/components/small-card/small-card.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit, } from '@angular/core';
 
 @Component({
   selector: 'app-small-card',
@@ -6,6 +6,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./small-card.component.css']
 })
 export class SmallCardComponent implements OnInit {
+
+  @Input()
+  photoCover:string ="https://media.seudinheiro.com/uploads/2023/02/o-senhor-dos-aneis.jpg"
+
+  @Input()
+  cardTitle: string ="Sociedade do Anel"
+
+  @Input()
+  Id:string="0"
+
 
   constructor() { }
 
---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,29 @@
+export const dataFake = [
+  {
+    "id": "1",
+    "title": "Senhor dos Anéis a melhor franquia ja criada",
+    "descripition": "",
+    "photoCover": "https://i0.wp.com/cloud.estacaonerd.com/wp-content/uploads/2022/01/19132749/senhor-dos-aneis-serie.jpg?fit=1280%2C853&ssl=1"
+  },
+
+  {
+    "id": "2",
+    "title": "Primeiro filme",
+    "descripition": "A Sociedade do Anel",
+    "photoCover": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSnEJGqR3yOFDNra74gSdjA8ftMo16bTpJpU9o-qtxZIHPVCwqTF4nEDQKiqsBIeCBbQvI&usqp=CAU"
+  },
+
+  {
+    "id": "3",
+    "title": "Segundo Filme",
+    "descripition": "As Duas Torres",
+    "photoCover": "https://cinepop.com.br/wp-content/uploads/2021/12/o-senhor-dos-aneis-as-duas-torres.jpg"
+  },
+
+  {
+    "id": "4",
+    "title": "O Retorno do Rei",
+    "descripition": "Terceiro Filme",
+    "photoCover": "https://hbomax-images.warnermediacdn.com/images/GXeF-mg4zu8PCwwEAADwj/tile?size=640x360&partner=hbomaxcom&v=a08b967c4ac8e078416f256dabe77335&host=art-gallery.api.hbo.com&language=pt-br"
+  }
+];
